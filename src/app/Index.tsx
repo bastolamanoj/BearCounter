112ca8f8e4e5c9e963208f89bc5f2fb323231b6a
bastolamanoj <manojbastola676@gmail.com>
2024-09-17 22:13:29 +0545

    Inital Setup

---
+++ src/app/Index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Greet from '../components/Greet';
+
+const Index = () => {
+  return (
+    <Greet />
+  )
+}
+
+export default Index;
